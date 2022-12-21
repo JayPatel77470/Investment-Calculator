e4a9f3127668f5d39d213a8d86daf98066cbf452
Jay Patel <jgpatel10@myseneca.ca>
2022-12-20 23:57:55 -0500

    Added line chart

--- components/Form.js
+++ components/Form.js
@@ -1,14 +1,14 @@
-import React, { use, useState } from 'react'
+import React, { use, useState, useEffect } from 'react'
 import Form from 'react-bootstrap/Form'
 import { Row, Col, Button } from 'react-bootstrap'
 import { useForm } from 'react-hook-form'
 import { InputGroup } from 'react-bootstrap'
 import { useAtom } from 'jotai'
-import { finalAmountAtom, totalContributionAtom, totalInterestAtom, warningAtom } from '../store.js'
+import { finalAmountAtom, totalContributionAtom, totalInterestAtom, warningAtom, labelsAtom, amountDataAtom, contributionDataAtom, interestDataAtom } from '../store.js'
 
 const Values = () => {
 
-    const { register, handleSubmit} = useForm({
+    const { register, handleSubmit } = useForm({
         defaultValues: {
             principalAmount: 10000,
             years: 10,
@@ -16,38 +16,81 @@ const Values = () => {
             annualContribution: 1000,
         }
     })
+
+
     const [finalAmount, setFinalAmount] = useAtom(finalAmountAtom)
     const [totalContribution, setTotalContribution] = useAtom(totalContributionAtom)
     const [totalInterest, setTotalInterest] = useAtom(totalInterestAtom)
     const [warning, setWarning] = useAtom(warningAtom)
+    const [chartLabels, setChartLabels] = useAtom(labelsAtom)
+    const [amountData, setAmountData] = useAtom(amountDataAtom)
+    const [contributionData, setContributionData] = useAtom(contributionDataAtom)
+    const [interestData, setInterestData] = useAtom(interestDataAtom)
 
-    async function submitForm(data) {
-        if (!data.annualReturn || !data.years || !data.principalAmount)
+    useEffect(() => {
+        submitForm({ principalAmount: 10000, years: 10, annualReturn: 10, annualContribution: 1000 });
+    }, [])
+
+
+    function submitForm(data) {
+        if (!data.annualReturn || !data.years || !data.principalAmount) {
             setWarning("Annual Return, Years and, Principal Amount are required.")
-        else
+            setChartLabels([0]);
+            setAmountData([0]);
+        } else {
             setWarning(false)
-            await calculateFinalAmount(data);
+            setChartLabels([`0 yr`]);
+            setAmountData([parseFloat(data.principalAmount)]);
+            setContributionData([0]);
+            setInterestData([0]);
+            calculateFinalAmount(data);
+        }
     }
 
-    async function calculateFinalAmount(data) {
+    function createChart(Amount, Time, Contribution, Interest) {
+        setChartLabels(curr => [...curr, `${Time} yr`]);
+        setAmountData(curr => [...curr, Amount]);
+        setContributionData(curr => [...curr, Contribution]);
+        setInterestData(curr => [...curr, Interest]);
+    }
+
+    function calculateFinalAmount(data) {
         var r = parseFloat(data.annualReturn) / 100;
         var t = parseFloat(data.years);
         var P = parseFloat(data.principalAmount);
         var C = parseFloat(data.annualContribution);
         (isNaN(C)) && (C = 0);
-        var A = P * (Math.pow(1 + r, t)) + (C * ((Math.pow(1 + r, t) - 1) / (r)));
-        (isNaN(A) && (A = P + (C*t)));
-        A = Math.round((A + Number.EPSILON) * 100) / 100;
-        var I = Math.round((A - P - (C * t) + Number.EPSILON) * 100) / 100;
-
+        var A;
+        var I;
+        var time = t / 10;
+        if (time != 0) {
+            while (time <= t) {
+                time = Math.round((time + Number.EPSILON) * 100) / 100;
+                A = P * (Math.pow(1 + r, time)) + (C * ((Math.pow(1 + r, time) - 1) / (r)));
+                (isNaN(A) && (A = P + (C * time)));
+                A = Math.round((A + Number.EPSILON) * 100) / 100;
+                I = Math.round((A - P - (C * time) + Number.EPSILON) * 100) / 100;
+                createChart(A, time, C * time, I);
+                time += t / 10;
+            }
+        } else {
+            A = P;
+            I = 0;
+        }
+        // A = P * (Math.pow(1 + r, t)) + (C * ((Math.pow(1 + r, t) - 1) / (r)));
+        // (isNaN(A) && (A = P + (C * t)));
+        // A = Math.round((A + Number.EPSILON) * 100) / 100;
+        // I = Math.round((A - P - (C * t) + Number.EPSILON) * 100) / 100;
+        // createChart(A, t);
         var finalAmount = A.toLocaleString("en-US");
         var totalContribution = (C * t).toLocaleString("en-US")
         var totalInterest = I.toLocaleString("en-US")
-
-        await setFinalAmount(finalAmount);
-        await setTotalContribution(totalContribution);
-        await setTotalInterest(totalInterest);
+        setFinalAmount(finalAmount);
+        setTotalContribution(totalContribution);
+        setTotalInterest(totalInterest);
     }
+
+
     return (
         <div>
             <Form className='form-control' onSubmit={handleSubmit(submitForm)}>
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,49 @@ import Form from '../components/Form'
 import Result from '../components/Result'
 import { Container } from 'react-bootstrap'
 import { useAtom } from 'jotai';
-import { warningAtom } from '../store';
+import { warningAtom, labelsAtom, amountDataAtom, contributionDataAtom, interestDataAtom } from '../store';
 import { Alert } from 'react-bootstrap';
+import { MDBContainer } from "mdbreact";
+import { Line } from "react-chartjs-2";
+import { registerables, Chart } from "chart.js";
+Chart.register(...registerables);
+import 'mdbreact/dist/css/mdb.css';
 
 export default function Home() {
   const [warning, setWarning] = useAtom(warningAtom);
+  const [chartLabels, setChartLabels] = useAtom(labelsAtom);
+  const [amountData, setAmountData] = useAtom(amountDataAtom);
+  const [contributionData, setContributionData] = useAtom(contributionDataAtom);
+  const [interestData, setInterestData] = useAtom(interestDataAtom);
+
+
+  const lineData = {
+    labels: chartLabels,
+    datasets: [
+      {
+        label: "Balance",
+        data: amountData,
+        fill: false,
+        backgroundColor: "rgba(6, 156,51, .3)",
+        borderColor: "#02b844"
+      },
+      {
+        label: "Contributions",
+        data: contributionData,
+        fill: false,
+        backgroundColor: "rgba(0, 137, 132, .2)",
+        borderColor: "rgba(50, 150, 255, 1)"
+      },
+      {
+        label: "Interest",
+        data: interestData,
+        fill: false,
+        backgroundColor: "#e697d9",
+        borderColor: "#a83295"
+      }
+    ]
+  }
+
   return (
     <Container>
       <table>
@@ -16,12 +54,15 @@ export default function Home() {
               <Form />
             </td>
             <td width={'10%'}></td>
-              <td valign='top'  >
+            <td valign='top'  >
               {warning ? (<><br /><Alert variant="danger">{warning}</Alert></>) : (<><h2>Results</h2><Result /></>)}
-              </td>
+            </td>
           </tr>
         </tbody>
       </table>
+      <MDBContainer style={{ width: '50%' }}>
+        <Line data={lineData} />
+      </MDBContainer>
     </Container>
   )
 }
